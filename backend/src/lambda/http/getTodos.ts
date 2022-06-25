180a54bf312ec3ef54b7cced2e4f60754c91fb48
anh.pham <anh.pham081188@gmail.com>
2022-06-25 21:51:40 +0700

    implemented getTodos.ts

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,19 +1,28 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+import { getTodosByUserId } from '../../helpers/todos'
 import { getUserId } from '../utils';
+import { createLogger } from '../../utils/logger'
 
-// TODO: Get all TODO items for a current user
+const logger = createLogger('getTodos')
+
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
 
-    return undefined
+    logger.info('Start getTodos process', { event })
+    const userId = getUserId(event)
+    const todos = await getTodosByUserId(userId)
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        items: todos
+      })
+    }
+  }
+)
 
 handler.use(
   cors({
